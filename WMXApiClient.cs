9c283f04b6326b70fc9fe7ed97df5d938db3f8cd
jcy1236 <jcy1236@naver.com>
2025-07-10 13:13:03 +0900

    code 정리

--- WMXApiClient.cs
+++ WMXApiClient.cs
@@ -1,5 +1,21 @@
 public class WMXApiClient : IWMXApiClient
 {
+    // Setin
+    public int SetInBit(int addr, int bit, byte data)
+    {
+        return Wmx3Sim.SetInBit(addr, bit, data);
+    }
+
+    public int SetInByte(int addr, byte data)
+    {
+        return Wmx3Sim.SetInByte(addr, data);
+    }
+
+    public int SetInBytes(int addr, int size, byte[] data)
+    {
+        return Wmx3Sim.SetInBytes(addr, size, data);
+    }
+
     // SetOut
     public int SetOutBit(int addr, int bit, byte data)
     {
@@ -14,6 +30,25 @@ public class WMXApiClient : IWMXApiClient
     public int SetOutBytes(int addr, int size, byte[] data)
     {
         return Wmx3Lib_Io.SetOutBytes(addr, size, data);
+    }
+
+    // GetIn
+    public int GetInBit(int addr, int bit, out byte data)
+    {
+        data = 0;
+        return Wmx3Lib_Io.GetInBit(addr, bit, ref data);
+    }
+
+    public int GetInByte(int addr, out byte data)
+    {
+        data = 0;
+        return Wmx3Lib_Io.GetInByte(addr, ref data);
+    }
+
+    public int GetInBytes(int addr, int size, out byte[] data)
+    {
+        data = new byte[size];
+        return Wmx3Lib_Io.GetInBytes(addr, size, ref data);
     }
 
     // GetOut
@@ -42,39 +77,8 @@ public class WMXApiClient : IWMXApiClient
 
 
 
-    // Setin
-    public int SetInBit(int addr, int bit, byte data)
-    {
-        return Wmx3Sim.SetInBit(addr, bit, data);
-    }
-
-    public int SetInByte(int addr, byte data)
-    {
-        return Wmx3Sim.SetInByte(addr, data);
-    }
-
-    public int SetInBytes(int addr, int size, byte[] data)
-    {
-        return Wmx3Sim.SetInBytes(addr, size, data);
-    }
+    
 
 
-    // GetIn
-    public int GetInBit(int addr, int bit, out byte data)
-    {
-        data = 0;
-        return Wmx3Lib_Io.GetInBit(addr, bit, ref data);
-    }
-
-    public int GetInByte(int addr, out byte data)
-    {
-        data = 0;
-        return Wmx3Lib_Io.GetInByte(addr, ref data);
-    }
-
-    public int GetInBytes(int addr, int size, out byte[] data)
-    {
-        data = new byte[size];
-        return Wmx3Lib_Io.GetInBytes(addr, size, ref data);
-    }
+    
 }
